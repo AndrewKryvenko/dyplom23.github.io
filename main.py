d526b25a33183e4839f23cb96cb2f34e8d47904a
AndrewKryvenko <124074819+AndrewKryvenko@users.noreply.github.com>
2024-04-01 18:36:10 +0300

    main.py

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
-import asyncio
-import json
-
-from aiogram import types
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.context import FSMContext
-from aiogram.filters.command import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-bot = Bot(token="6442089419:AAG4q9RlLlpJ7w4HEKusOqTXUA18MSMaK_w")
-dp = Dispatcher(bot)
+import asyncio
 
-
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message, state: FSMContext):
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    await message.answer("ТУТ ПОЧАТКОВИЙ ТЕКСТ ПРИ КОМАНДІ СТАРТ", reply_markup=keyboard, parse_mode="Markdown")
-
-
-@dp.message_handler()
-async def web_app(message: types.Message):
-    json_data = message.web_app_data
-    parsed_data = json.loads(json_data)
-    message_text = ""
-    for i, item in enumerate(parsed_data['items'], start=1):
-        position = int(item['id'].replace('item', ''))
-        message_text += f"Позиція {position}\n"
-        message_text += f"Вартість {item['price']}\n\n"
-        message_text += f"Кількість {item['quantity']}\n\n"
-
-    message_text += f"Загальна вартість: {parsed_data['totalPrice']}"
-
-    await bot.send_message(message.from_user.id, message_text)
-    await bot.send_message('-1002022582711', f"Нове замовлення\n{message_text}")
-
+bot = Bot(token='6442089419:AAG4q9RlLlpJ7w4HEKusOqTXUA18MSMaK_w')
+dp = Dispatcher(bot=bot)
 
 async def main():
-    await dp.start_polling()
-
+    from handlers import dp
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped!')
