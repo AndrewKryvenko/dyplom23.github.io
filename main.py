60b2790945b50b79052213ada4c8bf61a24b006a
AndrewKryvenko <124074819+AndrewKryvenko@users.noreply.github.com>
2024-04-01 17:42:17 +0300

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import asyncio
 import json
 from aiogram import types
+from aiogram.dispatcher.filters import CommandStart
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.context import FSMContext
-from aiogram.filters.command import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.dispatcher.filters import Command
 
 bot = Bot(token="6442089419:AAG4q9RlLlpJ7w4HEKusOqTXUA18MSMaK_w")
 dp = Dispatcher(bot)
 
+@dp.message_handler(CommandStart())
+async def start(message: types.Message):
+    item1 = types.KeyboardButton(text="Выбрать товар", url='https://andrewkryvenko.github.io/dyplom23.github.io/')
+    keyboard = types.ReplyKeyboardMarkup(keyboard=[[item1]], resize_keyboard=True)
+    await message.answer("Вітаємо! 🙌🏼\n Натисність на кнопку МЕНЮ знизу зліва щоб замовити їжу👇🏼", reply_markup=keyboard, parse_mode="Markdown")
 
-@dp.message_handler(Command('start'))
-async def start(message: types.Message, state: FSMContext):
-    item1 = KeyboardButton(text="Вибрати товар", web_app=WebAppInfo(url='https://andrewkryvenko.github.io/dyplom23.github.io/'))
-    keyboard = ReplyKeyboardMarkup(keyboard=[[item1]], resize_keyboard=True)
-    await bot.send_message(message.from_user.id, "Вітаємо! 🙌🏼\n Натисність на кнопку МЕНЮ знизу зліва щоб замовити їжу👇🏼", reply_markup=keyboard, parse_mode="Markdown")
 
-
-@dp.message_handler(content_types=types.ContentType.WEB_APP)
+@dp.message_handler()
 async def web_app(message: types.Message):
-    json_data = message.web_app_data
+    json_data = message.text
     parsed_data = json.loads(json_data)
     message_text = ""
     for i, item in enumerate(parsed_data['items'], start=1):
@@ -30,12 +28,17 @@ async def web_app(message: types.Message):
 
     message_text += f"Загальна вартість: {parsed_data['totalPrice']}"
 
-    await bot.send_message(message.from_user.id, f"{message_text}")
-    await bot.send_message('-1002022582711', f"Нове замовлення\n{message_text}")
+    await bot.send_message(message.from_user.id, f"""
+{message}
+""")
+    await bot.send_message('-1002022582711', f"""
+Нове замовлення\n
+{message_text}
+""")
 
 
 async def main():
-    await dp.start_polling()
+        await dp.start_polling()
 
 
 if __name__ == "__main__":
